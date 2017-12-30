d9bda5c6cef536fc914e9627e3aa01bf7a4c85c6
Ruslan Akbashev <ruslashev@gmail.com>
2017-12-30 19:57:43 +0300

    Get balance

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,44 @@
 #include <cstdio>
+#include <cmath>
 #include <iostream>
 #include "../thirdparty/cpr/include/cpr/cpr.h"
 #include "../thirdparty/json.hpp"
+#include "secretz.hh"
+
+using json = nlohmann::json;
+using money_t = long double;
+using std::cout;
+using std::endl;
+
+const auto auth = cpr::Authentication{ apikey, apisecret };
+
+std::string url(const std::string &x) {
+  return "https://api.hitbtc.com/api/2/" + x;
+}
+
+money_t num(const std::string &x) {
+  return std::stold(x);
+}
+
+bool feq(money_t a, money_t b, money_t epsilon = 0.0000000000000001L) {
+  return std::abs(a - b) < epsilon;
+}
+
+void balance() {
+  auto response = cpr::Get(url("trading/balance"), auth);
+  auto j = json::parse(response.text);
+  for (auto &e : j) {
+    money_t avail = num(e["available"]);
+    if (feq(avail, 0.))
+      continue;
+    cout << e["currency"] << ' ' << e["available"] << ' ' << avail << endl;
+  }
+}
 
 int main(int argc, char **argv) {
-  auto response = cpr::Get(cpr::Url{"https://httpbin.org/get"});
-  std::cout << response.text << std::endl;
-  auto json = nlohmann::json::parse(response.text);
-  std::cout << json.dump(4) << std::endl;
+  cout.precision(16);
+  balance();
+  // auto json = nlohmann::json::parse(response.text);
+  // std::cout << json.dump(2) << std::endl;
 }
 
