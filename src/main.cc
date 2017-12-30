cbdeb74a3b71268e116cae00f15258633f4bf7f9
Ruslan Akbashev <ruslashev@gmail.com>
2017-12-30 17:56:35 +0300

    Add nlohmanns json

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,12 @@
 #include <cstdio>
 #include <iostream>
 #include "../thirdparty/cpr/include/cpr/cpr.h"
-#include "../thirdparty/pjson.h"
+#include "../thirdparty/json.hpp"
 
 int main(int argc, char **argv) {
   auto response = cpr::Get(cpr::Url{"https://httpbin.org/get"});
   std::cout << response.text << std::endl;
-  pjson::document doc;
-  doc.deserialize_in_place(response.text);
+  auto json = nlohmann::json::parse(response.text);
+  std::cout << json.dump(4) << std::endl;
 }
 
