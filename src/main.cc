0d86bced5ed4cebea3fd412670845312f05ce5e5
Ruslan Akbashev <ruslashev@gmail.com>
2017-12-30 17:56:22 +0300

    Add pjson stub

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,12 @@
 #include <cstdio>
 #include <iostream>
 #include "../thirdparty/cpr/include/cpr/cpr.h"
+#include "../thirdparty/pjson.h"
 
 int main(int argc, char **argv) {
   auto response = cpr::Get(cpr::Url{"https://httpbin.org/get"});
   std::cout << response.text << std::endl;
+  pjson::document doc;
+  doc.deserialize_in_place(response.text);
 }
 
