3bedb708acb3321e8d4efcfb048b59815f351cf5
Ruslan Akbashev <ruslashev@gmail.com>
2017-12-30 16:32:20 +0300

    Add cpr

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,9 @@
 #include <cstdio>
+#include <iostream>
+#include "../thirdparty/cpr/include/cpr/cpr.h"
 
-int main() {
-  puts("XDD");
+int main(int argc, char **argv) {
+  auto response = cpr::Get(cpr::Url{"https://httpbin.org/get"});
+  std::cout << response.text << std::endl;
 }
 
