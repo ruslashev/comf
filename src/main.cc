b04dd89f99ff67e84434ec190d628540fb2e8540
Ruslan Akbashev <ruslashev@gmail.com>
2017-12-31 18:08:29 +0300

    Kinda add uws

--- src/main.cc
+++ src/main.cc
@@ -2,8 +2,11 @@
 #include "secretz.hh"
 #include "money.hh"
 #include "rest.hh"
+#include <uWS/uWS.h>
 
 using namespace std;
+
+#define die(...) do { printf(__VA_ARGS__); puts(""); exit(1); } while (0)
 
 bool verbose = true;
 
@@ -13,7 +16,7 @@ void balance() {
     money_t avail = num(e["available"]);
     if (feq(avail, 0.))
       continue;
-    cout << e["currency"].get<std::string>() << " " << avail << endl;
+    cout << e["currency"].get<string>() << " " << avail << endl;
   }
 }
 
@@ -31,9 +34,56 @@ void load_pair_symbols() {
         it->second.second << ")\n";
 }
 
+void explain_error(long err) {
+  string text = "Client emitted error on ";
+  switch (err) {
+    case 1:  text += "invalid URI"; break;
+    case 2:  text += "resolve failure"; break;
+    case 3:  text += "connection timeout (non-SSL)"; break;
+    case 5:  text += "connection timeout (SSL)"; break;
+    case 6:  text += "HTTP response without upgrade (non-SSL)"; break;
+    case 7:  text += "HTTP response without upgrade (SSL)"; break;
+    case 10: text += "poll error"; break;
+    case 11: text += "invalid protocol";
+      static int protocol_error_count = 0;
+      ++protocol_error_count;
+      if (protocol_error_count > 1)
+        die("%d errors emitted for one connection!", protocol_error_count);
+      break;
+    default:
+      die("%d should not emit error!", err);
+  }
+}
+
+void start_loop() {
+  const string url = "wss://api.hitbtc.com/api/2/ws";
+  uWS::Hub h;
+
+  h.onError([](void *user) {
+    explain_error((long)user);
+  });
+
+  h.onConnection([&h](uWS::WebSocket<uWS::CLIENT> *ws, uWS::HttpRequest req) {
+    printf("connect\n");
+    printf("can send\n");
+    h.getDefaultGroup<uWS::CLIENT>().close();
+  });
+
+  h.onDisconnection([](uWS::WebSocket<uWS::CLIENT> *ws, int code, char *message,
+        size_t length) {
+    printf("client close (%d): <%s>\n", code, string(message, length).c_str());
+  });
+
+  h.listen(3000);
+  h.connect(url, nullptr);
+  h.run();
+  puts("Falling through");
+}
+
 int main(int argc, char **argv) {
   cout.precision(16);
-  load_pair_symbols();
-  balance();
+  // load_pair_symbols();
+  // balance();
+  start_loop();
 }
 
