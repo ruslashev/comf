ffac830644a9ed8d03d636fa2a70bf2a0e2e5057
Ruslan Akbashev <ruslashev@gmail.com>
2017-12-31 14:11:59 +0300

    Loading pair symbols

--- src/main.cc
+++ src/main.cc
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+bool verbose = true;
+
 void balance() {
   json j = rest("trading/balance");
   for (auto &e : j) {
@@ -15,8 +17,23 @@ void balance() {
   }
 }
 
+map<string, pair<string, string>> symbols;
+
+void load_pair_symbols() {
+  json j = rest_public("symbol");
+  for (auto &e : j)
+    symbols[e["id"]] = pair<string, string>(e["baseCurrency"],
+        e["quoteCurrency"]);
+
+  if (verbose)
+    for (auto it = symbols.begin(); it != symbols.end(); ++it)
+      cout << it->first << " => (" << it->second.first << "," <<
+        it->second.second << ")\n";
+}
+
 int main(int argc, char **argv) {
   cout.precision(16);
+  load_pair_symbols();
   balance();
 }
 
